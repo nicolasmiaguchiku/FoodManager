4ef7c9731246bbac51fccbdebaa64645299c6bd5
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-01-31 02:27:11 -0300

    feat: created handlers and requests

---
+++ src/FoodManager.Catalog.Application/Input/Handlers/Commands/UploadFoodImageCommand.cs
@@ -0,0 +1,7 @@
+﻿using FoodManager.Catalog.Application.Input.Requests;
+using LiteBus.Commands.Abstractions;
+using Mattioli.Configurations.Models;
+
+namespace FoodManager.Catalog.Application.Input.Handlers.Commands;
+
+public sealed record UploadFoodImageCommand(Guid Id, UploadFoodImageRequest Request) : ICommand<Result>;
---
+++ src/FoodManager.Catalog.Application/Input/Handlers/Commands/UploadFoodImageCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Commands.Abstractions;
+using Mattioli.Configurations.Models;
+
+namespace FoodManager.Catalog.Application.Input.Handlers.Commands
+{
+    public sealed class UploadFoodImageCommandHandler() : ICommandHandler<UploadFoodImageCommand, Result>
+    {
+        public Task<Result> HandleAsync(UploadFoodImageCommand message, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
