f7d946b012bb1954efe7143ef0c816d45085bed7
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-01-31 01:32:10 -0300

    feat: create request for import image and get image

---
+++ src/FoodManager.Catalog.Application/Input/Requests/ImportImageFoodFileRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FoodManager.Catalog.Application.Input.Requests
+{
+    public record ImportImageFoodFileRequest
+    {
+        public required IFormFile File { get; set; }
+        public required string FileImage { get; set; }
+    }
+}
