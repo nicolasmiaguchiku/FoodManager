024f9940bd7a4958bf6445ae2923956611f5ca66
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-05-11 00:23:21 -0300

    feat: update package and change the price type from decimal to double. create entitie category and refactor food entity

---
+++ src/FoodManager.Catalog.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodManager.Catalog.Domain.Entities
+{
+    public class CategoryEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = default!;
+        public string Description { get; set; } = string.Empty;
+        public string Tenant { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
