535f582d9adbcbb27117eab19fe8d9b71fa66867
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-05-11 00:42:21 -0300

    feat: create interface category

---
+++ src/FoodManager.Catalog.Domain/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using FoodManager.Catalog.Domain.Entities;
+using FoodManager.Internal.Shared.Repositories;
+
+namespace FoodManager.Catalog.Domain.Interfaces.Repositories
+{
+    public interface ICategoryRepository : IBaseRepository<CategoryEntity>
+    {
+    }
+}
